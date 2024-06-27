c779828ca26c5bbe29de599c85e9b89c88d393aa
thegigacoder123 <muaazarkhan@gmail.com>
2024-06-27 13:10:42 +0500

    tweaked abit more

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = createApp()
 
 if __name__ == "__main__":
 
-    app.run()
+    app.run(host='0.0.0.0')
--- main/__init__.py
+++ main/__init__.py
@@ -1,10 +1,4 @@
 from flask import Flask
-from flask_login import LoginManager
-from os import path
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-DB_NAME = 'chatbase.db'
 
 def createApp():
     from .views import views
@@ -12,13 +6,7 @@ def createApp():
 
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "wowsosecret"
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     app.register_blueprint(views, url_prefix="/")
 
     return app
-
-def create_db(app):
-    if not path.exists('website/'+DB_NAME):
-        db.create_all(app=app)
-        print("Database Created")
